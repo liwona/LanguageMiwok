8a95c1ce65648567acc532dd5e5c636ec53cbcdb
liwona <liwona@users.noreply.github.com>
2018-03-25 15:57:34 +0200

    using while and for loop

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,11 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +14,72 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+//        //Create an array of words
+//        String[] words = new String[10];
+//        words[0] = "one";
+//        words[1] = "two";
+//        words[2] = "three";
+//        words[3] = "four";
+//        words[4] = "five";
+//        words[5] = "six";
+//        words[6] = "seven";
+//        words[7] = "eight";
+//        words[8] = "nine";
+//        words[9] = "ten";
+//
+//        // Verify the contents of the array by printing out each array element to the logs
+//        Log.v("NumbersActivity", "Word at index 0: " + words[0]);
+//        Log.v("NumbersActivity", "Word at index 1: " + words[1]);
+
+        //Create an list of words
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+//        // Verify the contents of the ArrayList by printing out each ArrayList element to the logs
+//        Log.v("NumbersActivity", "Word at index 0: " + words.get(0));
+//        Log.v("NumbersActivity", "Word at index 2: " + words.get(2));
+
+        // Find the root view so we can add child views to it
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+//        // Create a variable to keep track of the current index position
+//        int index = 0;
+//        // Keep looping until we've reached the end of the list (which means keep looping
+//        // as long as the current index position is less than the length of the list)
+//        while (index < words.size()){
+//            // Create a new TextView
+//            TextView wordView = new TextView(this);
+//
+//            // Set the text to be word at the current index
+//            wordView.setText(words.get(index));
+//
+//            // Add this TextView as another child to the root view of this layout
+//            rootView.addView(wordView);
+//
+//            //Increment the index variable by 1
+//            index++;
+//        }
+
+        for(int index = 0; index < words.size(); index++){
+            //Create a new TextView
+            TextView wordView = new TextView(this);
+
+            // Set the text to be word at the current index
+            wordView.setText(words.get(index));
+
+            // Add this TextView as another child to the root view of this layout
+            rootView.addView(wordView);
+        }
+
     }
 }
