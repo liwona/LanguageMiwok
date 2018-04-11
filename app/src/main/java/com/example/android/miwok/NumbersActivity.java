b968333d073adf9f23dd5e4449865a6cb326d3e4
liwona <liwona@users.noreply.github.com>
2018-04-11 23:52:57 +0200

    set onItemCLickListener to play the sound when item list clicked on

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
+    /*Handles playback of all the sound files*/
     private MediaPlayer mMediaPlayer;
 
     @Override
@@ -44,25 +45,25 @@ public class NumbersActivity extends AppCompatActivity {
         //poniższy kod można zapisać krócej, jak w linii 45;
 //        Word w = new Word("one", "lutti");
 //        words.add(w);
-        words.add(new Word("one", "lutti", R.drawable.number_one));
+        words.add(new Word("one", "lutti", R.drawable.number_one, R.raw.number_one));
 //        words.add("two");
-        words.add(new Word("two", "otiiko", R.drawable.number_two));
+        words.add(new Word("two", "otiiko", R.drawable.number_two, R.raw.number_two));
 //        words.add("three");
-        words.add(new Word("three", "tolookosu", R.drawable.number_three));
+        words.add(new Word("three", "tolookosu", R.drawable.number_three, R.raw.number_three));
 //        words.add("four");
-        words.add(new Word("four", "oyyisa", R.drawable.number_four));
+        words.add(new Word("four", "oyyisa", R.drawable.number_four, R.raw.number_four));
 //        words.add("five");
-        words.add(new Word("five", "massokka", R.drawable.number_five));
+        words.add(new Word("five", "massokka", R.drawable.number_five, R.raw.number_five));
 //        words.add("six");
-        words.add(new Word("six", "temmokka", R.drawable.number_six));
+        words.add(new Word("six", "temmokka", R.drawable.number_six, R.raw.number_six));
 //        words.add("seven");
-        words.add(new Word("seven", "kenekaku", R.drawable.number_seven));
+        words.add(new Word("seven", "kenekaku", R.drawable.number_seven, R.raw.number_seven));
 //        words.add("eight");
-        words.add(new Word("eight", "kawinta", R.drawable.number_eight));
+        words.add(new Word("eight", "kawinta", R.drawable.number_eight, R.raw.number_eight));
 //        words.add("nine");
-        words.add(new Word("nine", "wo’e", R.drawable.number_nine));
+        words.add(new Word("nine", "wo’e", R.drawable.number_nine, R.raw.number_nine));
 //        words.add("ten");
-        words.add(new Word("ten", "na’aacha", R.drawable.number_ten));
+        words.add(new Word("ten", "na’aacha", R.drawable.number_ten, R.raw.number_nine));
 
 
 
@@ -134,10 +135,15 @@ public class NumbersActivity extends AppCompatActivity {
         // {@link ListView} will display list items for each {@link Word} in the list.
         listView.setAdapter(adapter);
 
+        //Set a click listener to play the audio when the list item is clicked on
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                mMediaPlayer = MediaPlayer.create(NumbersActivity.this, R.raw.number_one);
+                // Get the {@link Word} object at the given position the user clicked on
+                Word song = words.get(position);
+                // Create and setup {@link MediaPlayer} for the audio resource associated with the current Word
+                mMediaPlayer = MediaPlayer.create(NumbersActivity.this, song.getAudioResourceId() );
+                // Start the audio file
                 mMediaPlayer.start();
             }
         });
--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -21,8 +21,8 @@ public class Word {
     /** Constant value that represents no image was provided for this word */
     private static final int NO_IMAGE_PROVIDED = -1;
 
-    /** Image resource ID for the word */
-    private int mImageResourceId = NO_IMAGE_PROVIDED;
+    /** Audio resource ID for the word */
+    private int mAudioResourceId;
 
     /**
      * Create a new Word object.
@@ -30,10 +30,12 @@ public class Word {
      * @param defaultTranslation is the word in a language that the user is already familiar with
      *                           (such as English)
      * @param miwokTranslation is the word in the Miwok language
+     * @param audioResourceId is the resource ID for audio file associated with this Word
      */
-    public Word(String defaultTranslation, String miwokTranslation) {
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceId) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+        mAudioResourceId =audioResourceId;
     }
 
     /**
@@ -43,11 +45,13 @@ public class Word {
      *                           (such as English)
      * @param miwokTranslation is the word in the Miwok language
      * @param imageResourceId is the drawable resource Id for the image associated with the Word
+     * @param audioResourceId is the resource ID for audio file associated with this Word
      */
-    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId){
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId, int audioResourceId){
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
         mImageResourceId = imageResourceId;
+        mAudioResourceId =audioResourceId;
     }
 
     /**
@@ -76,5 +80,12 @@ public class Word {
      */
     public boolean hasImage() {
         return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+
+    /**
+     * Return the audio resource ID of the word
+     */
+    public int getAudioResourceId() {
+        return mAudioResourceId;
     }
 }
